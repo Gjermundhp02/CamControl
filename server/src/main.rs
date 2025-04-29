cc063aeb2cf8e9d4dfe79144c76a05f89049f5a6
gjermundhp02 <gjermund.pedersen@gmail.com>
2025-04-29 09:59:52 +0200

    Temp

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,9 +1,11 @@
-use tokio::net::TcpListener;
+use interpolation::lerp;
+use tokio::{net::TcpListener, time::{sleep, sleep_until, Instant, Duration}};
 use tokio_tungstenite::accept_async;
 use tokio_tungstenite::tungstenite::protocol::Message;
 use anyhow::Result;
 use futures_util::{SinkExt, StreamExt};
-use std::sync::{Arc, Mutex};
+use std::{collections::HashMap, pin::Pin, sync::{atomic::{AtomicBool, Ordering}, Arc}};
+use tokio::sync::Mutex;
 
 mod soc_handler;
 use soc_handler::handle_connection;
@@ -12,13 +14,61 @@ struct State {
     position: Arc<Mutex<[u16; 2]>>, // Steps 
     velocity: Arc<Mutex<[i16; 2]>>, // Steps per second
     target_velocity: Arc<Mutex<[i16; 2]>>, // Steps per second
-    fire: Arc<Mutex<bool>>, // TODO: Timeout fire if no new command comes within 1 second
+    fire: Arc<AtomicBool>, // TODO: Timeout fire if no new command comes within 1 second
+    fire_timeout: Arc<Mutex<tokio::time::Instant>>, // TODO: Timeout fire if no new command comes within 1 second
 }
+
+impl State {
+    pub async fn fire(&self){
+        if self.fire.load(Ordering::Relaxed){
+            let mut timeout = self.fire_timeout.lock().await;
+            *timeout = Instant::now()+Duration::from_secs(1);
+        }
+        else {
+            self.fire.store(true, Ordering::Relaxed);
+            let timeout_clone = Arc::clone(&self.fire_timeout);
+            let fire_clone = Arc::clone(&self.fire);
+            tokio::spawn(async move {
+                let mut timeout = timeout_clone.lock().await;
+                *timeout = Instant::now()+Duration::from_secs(1);
+                let target = *timeout;
+                sleep_until(target).await;
+                if Instant::now() >= target {
+                    fire_clone.store(false, Ordering::Relaxed);
+                }
+            }); 
+        }
+    }
+    pub async fn set_target_velocity(&self, data: Vec<u8>) -> Result<()> {
+        let mut target_velocity = self.target_velocity.lock().await;
+        target_velocity[0] = lerp(&-CONFIG.max_velocity, &CONFIG.max_velocity, &(data[1] as f32 / 255.0)) as i16;
+        target_velocity[1] = lerp(&-CONFIG.max_velocity, &CONFIG.max_velocity, &(data[2] as f32 / 255.0)) as i16;
+        Ok(())
+    }
+}
+
+type Command = FnMut(Vec<u8>, &Arc<State>) -> impl std::future::Future<Output = Result<()>>;
 
 struct Config {
     steps_per_degree: u16,
     acceleration: u8,
     max_velocity: i16,
+    commands: HashMap<u8, Command>,
+}
+
+
+
+impl Config {
+    pub fn new() -> Self {
+        let mut commands: HashMap<u8, Command> = HashMap::new();
+        commands.insert(0, async |data: Vec<u8>, state: &Arc<State>| {
+            state.set_target_velocity(data).await?;
+        });
+        let func = async |data: Vec<u8>, state: &Arc<State>| {
+            state.set_target_velocity(data).await?;
+        };
+        Self { steps_per_degree: 100, acceleration: 100, max_velocity: 200, commands }
+    }
 }
 
 const CONFIG: Config = Config {
@@ -37,7 +87,8 @@ async fn main() -> Result<()> {
         position: Arc::new(Mutex::new([0, 0])), 
         velocity: Arc::new(Mutex::new([0, 0])), 
         target_velocity: Arc::new(Mutex::new([0, 0])),
-        fire: Arc::new(Mutex::new(false)) 
+        fire: Arc::new(AtomicBool::new(false)), 
+        fire_timeout: Arc::new(Mutex::new(tokio::time::Instant::now())), // Initialize fire_timeout
     });
 
     while let Ok((stream, _)) = listener.accept().await {
--- server/src/soc_handler/commands.rs
+++ server/src/soc_handler/commands.rs
@@ -1,6 +1,6 @@
-use std::{collections::HashMap, sync::{Arc, Mutex}};
+use std::{collections::HashMap, ops::DerefMut, sync::{atomic::Ordering, Arc}};
 use interpolation::lerp;
-use tokio::{sync::oneshot, time::{sleep, Duration}};
+use tokio::{sync::oneshot, time::{sleep, Duration}, sync::Mutex};
 
 use anyhow::{Ok, Result};
 
@@ -12,52 +12,11 @@ type CommandHandler = dyn Fn(Vec<u8>, &Arc<State>) -> Result<()>;
 
 type Commands = HashMap<u8, Box<CommandHandler>>;
 
-fn set_timeout<F>(duration: Duration, callback: F) -> impl FnOnce() -> tokio::task::JoinHandle<()>
-where
-    F: FnOnce() + Send + 'static,
-{
-    let (cancel_tx, cancel_rx) = oneshot::channel::<()>();
-    
-    // Return a function that cancels the timeout when called
-    move || {
-        tokio::spawn(async move {
-            tokio::select! {
-                _ = sleep(duration) => {
-                    // Timeout expired without cancellation
-                    callback();
-                },
-                _ = cancel_rx => {
-                    // Timeout was canceled before execution
-                    println!("Timeout canceled!");
-                },
-            }
-        })
-    }
-}
-
-fn set_target_velocity(data: Vec<u8>, state: &Arc<State>) -> Result<()> {
-    let mut target_velocity = state.target_velocity.lock().unwrap();
+async fn set_target_velocity(data: Vec<u8>, state: &Arc<State>) -> Result<()> {
+    let mut target_velocity = state.target_velocity.lock().await;
     target_velocity[0] = lerp(&-CONFIG.max_velocity, &CONFIG.max_velocity, &(data[1] as f32 / 255.0)) as i16;
     target_velocity[1] = lerp(&-CONFIG.max_velocity, &CONFIG.max_velocity, &(data[2] as f32 / 255.0)) as i16;
     Ok(())
-}
-
-fn fire(_data: Vec<u8>, state: &Arc<State>) -> impl Fn(Vec<u8>, &Arc<State>) -> Result<()> {
-    let timeout = tokio::time::timeout(Duration::from_secs(60), async {
-        sleep(Duration::from_secs(1)).await;
-        let mut fire = state.fire.lock().unwrap();
-        *fire = false;
-    });
-    
-    return |_data: Vec<u8>, state: &Arc<State>| -> Result<()> {
-        let _ = set_timeout(Duration::from_secs(60), || {
-            let mut fire = state.fire.lock().unwrap();
-            *fire = false;
-        });
-        let mut fire = state.fire.lock().unwrap();
-        *fire = true;
-        Ok(())
-    };
 }
 
 pub fn get_commands() -> Commands {
@@ -68,6 +27,8 @@ pub fn get_commands() -> Commands {
 
 #[cfg(test)]
 mod tests {
+    use std::sync::atomic::AtomicBool;
+
     use super::*;
 
     fn new_state() -> Arc<State> {
@@ -75,16 +36,17 @@ mod tests {
             position: Arc::new(Mutex::new([0, 0])), 
             velocity: Arc::new(Mutex::new([0, 0])), 
             target_velocity: Arc::new(Mutex::new([0, 0])),
-            fire: Arc::new(Mutex::new(false)) 
+            fire: Arc::new(AtomicBool::new(false)),
+            fire_timeout: Arc::new(Mutex::new(tokio::time::Instant::now()))
         })
     }
 
-    #[test]
-    fn test_set_speed() -> Result<()> {
+    #[tokio::test]
+    async fn test_set_speed() -> Result<()> {
         let state = new_state();
 
         set_target_velocity(vec![0, 0, 255], &state)?;
-        let speed = state.velocity.lock().unwrap();
+        let speed = state.velocity.lock().await;
         assert_eq!(speed[0], -200);
         assert_eq!(speed[1], CONFIG.max_velocity);
         Ok(())
